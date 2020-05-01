64461a2e4926d5378b50a681f4afeaa61d7290f3
Szymon Wygnanski <s@finalclass.net>
2020-05-01 17:53:10 +0200

    rotmg like characters

--- src/scripts/objects/player-sprite.ts
+++ src/scripts/objects/player-sprite.ts
@@ -3,6 +3,15 @@ import { SwordSprite } from "./sword-sprite"
 
 const VELOCITY = 200;
 
+interface ActionConfig {
+    when: Array<Phaser.Input.Keyboard.Key>;
+    anim: 'scolar-walk-up'|'scolar-walk-right'|'scolar-walk-down'|'scolar-walk-left'|
+        'scolar-stand-up'|'scolar-stand-right'|'scolar-stand-down'|'scolar-stand-left';
+    facing: 'up'|'down'|'left'|'right';
+    velocityX: number,
+    velocityY: number
+}
+
 export class PlayerSprite extends Phaser.Physics.Arcade.Sprite {
 
     UpKey: Phaser.Input.Keyboard.Key;
@@ -11,11 +20,16 @@ export class PlayerSprite extends Phaser.Physics.Arcade.Sprite {
     RightKey: Phaser.Input.Keyboard.Key;
     body: Phaser.Physics.Arcade.Body;
 
+    facing:'up'|'down'|'left'|'right' = 'down';
+
     sword: SwordSprite;
     mousePointer: Phaser.Input.Pointer | null;
 
+    actionConfigs: ActionConfig[]
+
     constructor(scene: Phaser.Scene, x: number, y: number) {
-        super(scene, x * 32, y * 32, 'images', 3806);
+        super(scene, x * 32, y * 32, 'scolar', 0);
+        this.setScale(0.2);
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -33,32 +47,110 @@ export class PlayerSprite extends Phaser.Physics.Arcade.Sprite {
         this.scene.input.on('pointerdown', () => {
             this.sword.hit();
         });
+        this.updateAnimation = this.updateAnimation.bind(this);
+        
+        this.DownKey.on('down', this.updateAnimation).on('up', this.updateAnimation);
+        this.UpKey.on('down', this.updateAnimation).on('up', this.updateAnimation);
+        this.LeftKey.on('down', this.updateAnimation).on('up', this.updateAnimation);
+        this.RightKey.on('down', this.updateAnimation).on('up', this.updateAnimation);
         
         this.sword = new SwordSprite(scene);
+        
+        const velocitySqrt = Math.sqrt(VELOCITY * VELOCITY / 2);
+        this.actionConfigs = [
+            {
+                when: [this.UpKey],
+                anim: 'scolar-walk-up',
+                facing: 'up',
+                velocityY: -VELOCITY,
+                velocityX: 0
+            },
+            {
+                when: [this.UpKey, this.RightKey],
+                anim: 'scolar-walk-up',
+                facing: 'up',
+                velocityY: -velocitySqrt,
+                velocityX: velocitySqrt
+            },
+            {
+                when: [this.RightKey],
+                anim: 'scolar-walk-right',
+                facing: 'right',
+                velocityY: 0,
+                velocityX: VELOCITY
+            },
+            {
+                when: [this.RightKey, this.DownKey],
+                anim: 'scolar-walk-right',
+                facing: 'right',
+                velocityY: velocitySqrt,
+                velocityX: velocitySqrt
+            },
+            {
+                when: [this.DownKey],
+                anim: 'scolar-walk-down',
+                facing: 'down',
+                velocityY: VELOCITY,
+                velocityX: 0
+            },
+            {
+                when: [this.LeftKey, this.DownKey],
+                anim: 'scolar-walk-left',
+                facing: 'left',
+                velocityY: velocitySqrt,
+                velocityX: -velocitySqrt
+            },
+            {
+                when: [this.LeftKey],
+                anim: 'scolar-walk-left',
+                facing: 'left',
+                velocityY: 0,
+                velocityX: -VELOCITY
+            },
+            {
+                when: [this.LeftKey, this.UpKey],
+                anim: 'scolar-walk-left',
+                facing: 'left',
+                velocityY: -velocitySqrt,
+                velocityX: -velocitySqrt
+            }
+        ];
+    }
+
+    getActionConfig() : ActionConfig|undefined {
+        return this.actionConfigs
+            .filter(cfg => cfg.when.every(key => key.isDown))
+            .sort((a, b) => b.when.length - a.when.length)[0];
+    }
+
+    updateAnimation() {
+        const actionConfig = this.getActionConfig();
+
+        if (actionConfig) {
+            this.facing = actionConfig.facing;
+            this.play(actionConfig.anim);
+        } else {
+            if (this.facing === 'up') {
+                this.play('scolar-stand-up');
+            } else if (this.facing === 'right') {
+                this.play('scolar-stand-right');
+            } else if (this.facing === 'left') {
+                this.play('scolar-stand-left');
+            } else {
+                this.play('scolar-stand-down');
+            }
+        }
     }
 
     update() {
-        // if both keys are pressed, lower the velocity
-        const velocity = (this.UpKey.isDown || this.DownKey.isDown) && (this.LeftKey.isDown || this.RightKey.isDown)
-            ? Math.sqrt(VELOCITY * VELOCITY / 2)
-            : VELOCITY;
-         
-        if (this.UpKey.isDown) {
-            this.body.setVelocityY(-velocity);
-        } else if (this.DownKey.isDown) {
-            this.body.setVelocityY(velocity);
-        } else {
-            this.body.setVelocityY(0);
-        }
+        const actionConfig = this.getActionConfig();
 
-        if (this.LeftKey.isDown) {
-            this.body.setVelocityX(-velocity);
-        } else if (this.RightKey.isDown) {
-            this.body.setVelocityX(velocity);
+        if (actionConfig) {
+            this.body.setVelocity(actionConfig.velocityX, actionConfig.velocityY);
         } else {
-            this.body.setVelocityX(0);
+            this.body.setVelocity(0, 0);
         }
-
+        
         this.sword.updatePosition(this.body.position, this.mousePointer);
     }
     
--- src/scripts/scenes/main-scene.ts
+++ src/scripts/scenes/main-scene.ts
@@ -12,35 +12,92 @@ export default class MainScene extends Phaser.Scene {
         super({ key: "MainScene" });
 
         this.level = [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 2, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 2, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
         
     }
 
     preload() {
+        this.load.spritesheet("scolar", "/assets/img/scolar.png", {
+            frameWidth: 160,
+            frameHeight: 160,
+        })
         this.load.spritesheet("images", "/assets/img/ProjectUtumno_full.png", {
             frameWidth: 32,
             frameHeight: 32
         });
     }
 
+    createAnimations() {
+        const frameRate = 4;
+        const repeat = -1
+        
+        this.anims.create({
+            key: "scolar-stand-down",
+            frames: this.anims.generateFrameNames('scolar', { start: 0, end: 0 }),
+            frameRate, repeat
+        })
+        
+        this.anims.create({
+            key: "scolar-walk-down",
+            frames: this.anims.generateFrameNames('scolar', { start: 1, end: 2 }),
+            frameRate, repeat
+        });
+
+        this.anims.create({
+            key: "scolar-stand-left",
+            frames: this.anims.generateFrameNames('scolar', { start: 3, end: 3 }),
+            frameRate, repeat
+        });
+        
+        this.anims.create({
+            key: "scolar-walk-left",
+            frames: this.anims.generateFrameNames('scolar', { start: 3, end: 4 }),
+            frameRate, repeat
+        });
+
+        this.anims.create({
+            key: "scolar-stand-right",
+            frames: this.anims.generateFrameNames('scolar', { start: 5, end: 5 }),
+            frameRate, repeat
+        });
+        
+        this.anims.create({
+            key: "scolar-walk-right",
+            frames: this.anims.generateFrameNames('scolar', { start: 5, end: 6 }),
+            frameRate, repeat
+        });
+
+        this.anims.create({
+            key: "scolar-stand-up",
+            frames: this.anims.generateFrameNames('scolar', { start: 7, end: 7 }),
+            frameRate, repeat
+        });
+        
+        this.anims.create({
+            key: "scolar-walk-up",
+            frames: this.anims.generateFrameNames('scolar', { start: 8, end: 9 }),
+            frameRate, repeat
+        });
+    }
+    
     create() {
         const { width, height } = this.cameras.main;
         
         this.background = this.add.tileSprite(0, 0, width, height, "images", 219)
         this.background.setOrigin(0, 0);
 
-        // this.player = new PlayerSprite(this, );
+        this.createAnimations();
 
         this.walls = this.physics.add.staticGroup();
-        this.level.forEach((row, x) => {
-            row.forEach((value, y) => {
+        this.level.forEach((row, y) => {
+            row.forEach((value, x) => {
                 if (value === 1) {
                     this.walls.create(x * 32, y * 32, "images", 266);
                 }
@@ -50,6 +107,8 @@ export default class MainScene extends Phaser.Scene {
             });
         });
 
+        this.player.play('scolar-stand-still');
+        
         this.physics.add.collider(this.player, this.walls);
     }
 
